a4c25f82c34374b9573db4d9b539491a2e275c34
zephyrus <joelogira@gmail.com>
2024-02-02 22:33:53 +0300

    Added Header and myprofile pic

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import Header from "./components/Header";
+import MyProfilePic from "./components/MyProfilePic";
 
 export const metadata: Metadata = {
   title: "My Profile",
@@ -13,7 +15,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="dark:bg-slate-800">{children}</body>
+      <body className="dark:bg-slate-800">
+        <Header />
+        <MyProfilePic />
+        {children}
+      </body>
     </html>
   );
 }
